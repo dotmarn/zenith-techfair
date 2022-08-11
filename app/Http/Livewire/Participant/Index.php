f8f93b990d971c9a354cf78e8de8a679000eedcc
Kasim Ridwan <kasimsheyi20@gmail.com>
2022-08-11 13:18:40 +0100

    recent changes

--- app/Http/Livewire/Participant/Index.php
+++ app/Http/Livewire/Participant/Index.php
@@ -61,49 +61,49 @@ class Index extends Component
     {
         $have_an_account_rule = ($this->have_an_account == "yes") ? ['required', 'numeric', 'digits:10'] : ['nullable'];
 
-        // $this->validate([
-        //     'firstname' => ['required', 'string', 'min:3'],
-        //     'lastname' => ['required', 'string', 'min:3'],
-        //     'email' => ['required', 'string', 'email', 'unique:registrations,email'],
-        //     'job_function' => ['required', 'string', Rule::in($this->job_functions)],
-        //     'phone' => ['required', 'numeric', 'digits:11', 'unique:registrations,phone'],
-        //     'gender' => ['required', 'string'],
-        //     'have_an_account' => ['required', Rule::in(['yes', 'no'])],
-        //     'account_number' => $have_an_account_rule,
-        //     'industry_type' => ['required', 'string', Rule::in($this->industries)],
-        //     'area_of_responsibility' => ['required', 'string'],
-        //     'selectedInterests' => ['required', 'array']
-        // ], [
-        //     'have_an_account.required' => "This field is required",
-        //     'selectedInterests.required' => "This field is required"
-        // ]);
+        $this->validate([
+            'firstname' => ['required', 'string', 'min:3'],
+            'lastname' => ['required', 'string', 'min:3'],
+            'email' => ['required', 'string', 'email', 'unique:registrations,email'],
+            'job_function' => ['required', 'string', Rule::in($this->job_functions)],
+            'phone' => ['required', 'numeric', 'digits:11', 'unique:registrations,phone'],
+            'gender' => ['required', 'string'],
+            'have_an_account' => ['required', Rule::in(['yes', 'no'])],
+            'account_number' => $have_an_account_rule,
+            'industry_type' => ['required', 'string', Rule::in($this->industries)],
+            'area_of_responsibility' => ['required', 'string'],
+            'selectedInterests' => ['required', 'array']
+        ], [
+            'have_an_account.required' => "This field is required",
+            'selectedInterests.required' => "This field is required"
+        ]);
 
         $token = "ZEN-".Str::random(5)."-".mt_rand(1000, 9999);
 
-        // $registration = Registration::create([
-        //     'firstname' => $this->firstname,
-        //     'lastname' => $this->lastname,
-        //     'email' => $this->email,
-        //     'job_function' => $this->job_function,
-        //     'phone' => $this->phone,
-        //     'gender' => $this->gender,
-        //     'have_an_account' => $this->have_an_account,
-        //     'account_number' => $this->account_number,
-        //     'industry_type' => $this->industry_type,
-        //     'area_of_responsibility' => $this->area_of_responsibility,
-        //     'interests' => json_encode($this->selectedInterests)
-        // ]);
+        $registration = Registration::create([
+            'firstname' => $this->firstname,
+            'lastname' => $this->lastname,
+            'email' => $this->email,
+            'job_function' => $this->job_function,
+            'phone' => $this->phone,
+            'gender' => $this->gender,
+            'have_an_account' => $this->have_an_account,
+            'account_number' => $this->account_number,
+            'industry_type' => $this->industry_type,
+            'area_of_responsibility' => $this->area_of_responsibility,
+            'interests' => json_encode($this->selectedInterests)
+        ]);
 
         $image = \QrCode::size(500)->format('png')->generate(config('app.url').$token);
 
         $base64 = "data:image/png;base64,".base64_encode($image);
         $this->qr_code_url = Cloudinary::upload($base64)->getSecurePath();
 
-        // VerificationCode::create([
-        //     'registration_id' => $registration->id,
-        //     'qrcode_url' => $this->qr_code_url,
-        //     'token' => $token
-        // ]);
+        VerificationCode::create([
+            'registration_id' => $registration->id,
+            'qrcode_url' => $this->qr_code_url,
+            'token' => $token
+        ]);
 
         $this->step_one = false;
         $this->final_step = true;
