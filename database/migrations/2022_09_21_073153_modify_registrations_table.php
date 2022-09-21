a70b995360d2ba47a361360d94a70f0c6d4e34dd
Kasim Ridwan <kasimsheyi20@gmail.com>
2022-09-21 08:44:46 +0100

    added dbal to modify columns

---
+++ database/migrations/2022_09_21_073153_modify_registrations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('registrations', function (Blueprint $table) {
+            $table->longText('social_media')->after('interests')->nullable();
+            $table->string('role', 50)->nullable()->change();
+            $table->string('sector', 50)->nullable()->change();
+            $table->string('reason')->nullable()->change();
+            $table->text('interests')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('registrations', function (Blueprint $table) {
+            //
+        });
+    }
+};
