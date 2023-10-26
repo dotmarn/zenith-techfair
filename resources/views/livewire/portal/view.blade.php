929f3adeffc18f56f18b5e03c432abbb86c27be5
Kasim Ridwan <kasimsheyi20@gmail.com>
2023-10-26 11:30:48 +0100

    added print option

--- resources/views/livewire/portal/view.blade.php
+++ resources/views/livewire/portal/view.blade.php
@@ -13,6 +13,22 @@
     <div class="grid grid-cols-1 lg:grid-cols-3 lg:gap-x-8 gap-y-5 mb-4">
         <div class="lg:col-span-2">
             <div class="p-4 bg-white rounded-lg mb-4">
+                <div>
+                    <a href="javascript:void(0);" class="flex items-center place-content-end" onclick="printPageArea()">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0110.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0l.229 2.523a1.125 1.125 0 01-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0021 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 00-1.913-.247M6.34 18H5.25A2.25 2.25 0 013 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 011.913-.247m10.5 0a48.536 48.536 0 00-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5zm-3 0h.008v.008H15V10.5z" />
+                        </svg>
+                    </a>
+
+                    <div id="printableArea" class="hidden">
+                        <h2 class="font-semibold">
+                            {{ ucfirst($details->reg_info->firstname) . ' ' . ucfirst($details->reg_info->lastname) }}
+                        </h2>
+                    </div>
+                </div>
+
                 <div class="text-center">
                     <div class="mb-4">
                         <img src="/assets/images/default-avatar.jpg" alt="" srcset=""
@@ -241,8 +257,7 @@
 
                         @if ($session->status == 'pending')
                             <div class="text-center">
-                                <button type="button"
-                                    class="mx-auto bg-red-600 text-white py-2 px-6 rounded-lg"
+                                <button type="button" class="mx-auto bg-red-600 text-white py-2 px-6 rounded-lg"
                                     wire:click.prevent="checkIn({{ $session }})">Check In
                                     <span wire:loading wire:target="checkIn">
                                         <i class="fas fa-spinner fa-spin"></i>
@@ -276,5 +291,14 @@
                 });
             });
         });
+
+        function printPageArea()
+        {
+            var printContent = document.getElementById('printableArea').innerHTML;
+            var originalContent = document.body.innerHTML;
+            document.body.innerHTML = printContent;
+            window.print();
+            document.body.innerHTML = originalContent;
+        }
     </script>
 @endsection
